04160b0febac96a9b32e53907002b246d6e41149
Marcelo Pasut <pasutmarcelo@gmail.com>
2017-12-14 00:40:08 -0300

    agrego usuario y password de la base de datos

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 export DB_URI=jdbc:mysql://localhost/lazuly-users
+export DB_USERNAME=root
+export DB_PASSWORD=
 export HOST=http://localhost
 export JWT_SECRET=jwtsecret
 export INTERNAL_SECRET=internalsecret
