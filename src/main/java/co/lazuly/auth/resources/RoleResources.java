9725b37cfc8a46d389babed3ad2661d2f8035989
Marcelo Pasut <pasutmarcelo@gmail.com>
2017-12-26 11:48:00 -0300

    implemento consulta de multiples rules

--- src/main/java/co/lazuly/auth/resources/RoleResources.java
+++ src/main/java/co/lazuly/auth/resources/RoleResources.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 /**
@@ -27,5 +29,13 @@ public class RoleResources {
         log.info("Getting role {}", code);
         Role role = repo.findOne(code);
         return ResponseEntity.ok(role);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    ResponseEntity<Iterable<Role>> get(@RequestParam final List<String> codes) {
+        log.info("Getting roles for {}", codes);
+
+        Iterable<Role> roles = repo.findAll(codes);
+        return ResponseEntity.ok(roles);
     }
 }
